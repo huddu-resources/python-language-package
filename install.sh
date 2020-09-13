3d007964f905465abf54b0f99fa17439b79560f6
huddu-resources <70753177+huddu-resources@users.noreply.github.com>
2020-09-13 18:41:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "==> apt update"
 apt update
+echo "==> apt install python3-pip"
 yes | apt install python3-pip
+echo "==> apt apt install python-pip"
 yes | apt install python-pip
